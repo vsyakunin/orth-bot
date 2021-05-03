73e81ba98fe0292ca0df7bf11a6c783656fe5916
Vsevolod Yakunin <v.yakunin@it.ecommpay.com>
2021-05-03 18:32:14 +0300

    give it a start

--- main.go
+++ main.go
@@ -7,12 +7,42 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var (
+	inlineBtnProceedStart = tb.InlineButton{
+		Unique: "proceedStart",
+		Text:   "Дальше",
+	}
+
+	inlineBtnLetsStart = tb.InlineButton{
+		Unique: "letsStart",
+		Text:   "Начнём",
+	}
+
+	inlineBtn5min = tb.InlineButton{
+		Unique: "btn5min",
+		Text:   "5 минут",
+	}
+
+	inlineBtn15min = tb.InlineButton{
+		Unique: "btn15min",
+		Text:   "15 минут",
+	}
+
+	inlineBtn30min = tb.InlineButton{
+		Unique: "btn30min",
+		Text:   "30 минут",
+	}
+
+	inlineBtn1h = tb.InlineButton{
+		Unique: "btn1h",
+		Text:   "Час и более",
+	}
+)
+
+
 func main() {
 	b, err := tb.NewBot(tb.Settings{
-		// You can also set custom API URL.
-		// If field is empty it equals to "https://api.telegram.org".
-
-		Token:  "TOKEN_HERE",
+		Token:  "",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 
@@ -21,8 +51,51 @@ func main() {
 		return
 	}
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hello World!")
+	inlineKeys := [][]tb.InlineButton{
+		{inlineBtnProceedStart},
+	}
+
+	b.Handle(tb.OnText, func(m *tb.Message) {
+		b.Send(m.Sender, `Господи, прости!
+
+Ты столкнулся с проблемой зависимости от порнографии? Твоё сердце разъедает чувство вины за совершённое блудодеяние, гложет совесть? 
+
+Этот чат-бот подскажет тебе, как правильно обратиться к Господу, чтобы он помог отпустить этот грех.`,
+			&tb.ReplyMarkup{InlineKeyboard: inlineKeys, OneTimeKeyboard: true})
+	})
+
+	inlineKeysLetsStart := [][]tb.InlineButton{
+		{inlineBtnLetsStart},
+	}
+
+	b.Handle(&inlineBtnProceedStart, func(c *tb.Callback) {
+		b.Respond(c, &tb.CallbackResponse{ShowAlert: false})
+
+		b.Send(c.Sender, `Конечно, лучше всего отправиться на исповедь в церковь. Но, если такой возможности нет, или ты пока не решаешься, пусть этот бот будет твоим помощником. 
+
+Проект разработан SMIT.Studio совместно с православным иереем Константином Мальцевым. Отец Константин лично отобрал подходящие молитвы и благословил проект.`,
+			&tb.ReplyMarkup{InlineKeyboard: inlineKeysLetsStart, OneTimeKeyboard: true})
+	})
+
+	timeKeys := [][]tb.InlineButton{
+		{inlineBtn5min},
+		{inlineBtn15min},
+		{inlineBtn30min},
+		{inlineBtn1h},
+	}
+
+	b.Handle(&inlineBtnLetsStart, func(c *tb.Callback) {
+		b.Respond(c, &tb.CallbackResponse{ShowAlert: false})
+
+		b.Send(c.Sender, `Итак, ты поддался искушению и потратил своё время на просмотр порнографии. Возможно, ты чувствуешь вину, не знаешь, как облегчить свою душу и очистить разум? 
+
+Мы предлагаем тебе уделить для молитвы столько же времени, сколько ты потратил на просмотр порнографии.
+
+Просто ответь на этот простой вопрос
+
+Сколько времени ты потратил сейчас на порно? 
+`,
+			&tb.ReplyMarkup{InlineKeyboard: timeKeys, OneTimeKeyboard: true})
 	})
 
 	b.Start()
